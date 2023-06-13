654cf45319d9e21ce0ea0f639f91873ec27d0254
OuzelTheBird <archallica@gmail.com>
2023-06-13 17:30:51 +0300

    refactor: fix mypy checks
    This commit is a DRAFT one! The project doesn't run locally because the "secrets" folder and the .env files inside it are missing.

--- bot/handlers/common_handlers.py
+++ bot/handlers/common_handlers.py
@@ -10,7 +10,7 @@ from config import USERS_TABLE
 from bot.db.database import upsert_final_data_to_db, collect_initial_data_from_user
 from bot.dependencies import form_router
 from bot.fsm import CommonForm
-from bot.lang_ru import RU_COMMON_HANDLERS
+from bot.lang_ru import RU_COMMON_HANDLERS, RU_COMMON_HANDLERS_BUTTONS
 from bot.utils.misc import show_dev_summary
 
 
@@ -20,7 +20,7 @@ async def command_start(message: Message, state: FSMContext) -> None:
     await collect_initial_data_from_user(message, state)
     await state.set_state(CommonForm.role)
     kb_builder = await create_buttons(["Создать профиль"])
-    await message.answer(text=RU_COMMON_HANDLERS['command_start'],
+    await message.answer(text=str(RU_COMMON_HANDLERS['command_start']),
                          reply_markup=kb_builder.as_markup(resize_keyboard=True), )
 
 
@@ -34,14 +34,14 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
     logging.info("Cancelling state %r", current_state)
     await state.clear()
     await message.answer(
-        text=RU_COMMON_HANDLERS['cancel_handler'], reply_markup=ReplyKeyboardRemove(), )
+        text=RU_COMMON_HANDLERS['cancel_handler'], reply_markup=ReplyKeyboardRemove(remove_keyboard=True), )
 
 
 # РОЛЬ
 @form_router.message(CommonForm.role, F.text.casefold() == "создать профиль")
 async def get_role(message: Message, state: FSMContext) -> None:
     await state.set_state(CommonForm.experience)
-    kb_builder = await create_buttons(RU_COMMON_HANDLERS['roles'], width=3)
+    kb_builder = await create_buttons(RU_COMMON_HANDLERS_BUTTONS['roles'], width=3)
     await message.answer(
         text=RU_COMMON_HANDLERS['get_specialization'],
         reply_markup=kb_builder.as_markup(resize_keyboard=True), )
@@ -52,19 +52,19 @@ async def get_role(message: Message, state: FSMContext) -> None:
 async def indicate_experience(message: Message, state: FSMContext) -> None:
     await state.set_state(CommonForm.tech_stack)
     await state.update_data(role=message.text)
-    kb_builder = await create_buttons(RU_COMMON_HANDLERS['choose_experience'], width=3)
+    kb_builder = await create_buttons(RU_COMMON_HANDLERS_BUTTONS['choose_experience'], width=3)
     await message.answer(
         text=RU_COMMON_HANDLERS['check_experience'],
         reply_markup=kb_builder.as_markup(resize_keyboard=True), )
 
 
 # СТЕК
-@form_router.message(CommonForm.tech_stack, F.text.in_({*RU_COMMON_HANDLERS['choose_experience']}))
+@form_router.message(CommonForm.tech_stack, F.text.in_({*RU_COMMON_HANDLERS_BUTTONS['choose_experience']}))
 async def choose_tech_stack(message: Message, state: FSMContext) -> None:
     await state.set_state(CommonForm.summary)
     await state.update_data(experience=message.text)
     await message.answer(
-        text=RU_COMMON_HANDLERS['choose_tech_stack'], reply_markup=ReplyKeyboardRemove(), )
+        text=RU_COMMON_HANDLERS['choose_tech_stack'], reply_markup=ReplyKeyboardRemove(remove_keyboard=True), )
 
 
 # SUMMARY
@@ -80,7 +80,7 @@ async def get_summary(message: Message, state: FSMContext) -> None:
 # ССЫЛКА НА КАНАЛ
 @form_router.message(CommonForm.telegram_link, F.text.casefold() == "подтвердить")
 async def finish(message: Message, state: FSMContext) -> None:
-    await message.answer('Спасибо, что заполнил профиль!', reply_markup=ReplyKeyboardRemove())
+    await message.answer('Спасибо, что заполнил профиль!', reply_markup=ReplyKeyboardRemove(remove_keyboard=True))
     await state.update_data(submit='yes')
 
     kb_builder = await create_url_button("Peredelano Startups", "https://t.me/peredelanoconfjunior")
