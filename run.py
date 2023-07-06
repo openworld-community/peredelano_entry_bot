57c5074ead1ce2ace6a37f3e9cf2c6e5c3383996
arturkalimullin <noti1996@gmail.com>
2023-07-06 17:26:19 +0300

    feat: add cache invalidation

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import asyncio
+
+from bot.dependencies import dp, bot, scheduler
 from bot.handlers import admin_handlers
-from bot.handlers import user_handlers
-from bot.handlers import other_handlers
 from bot.handlers import misc_handlers
-from bot.dependencies import dp, bot
-from bot.utils.misc import check_eventloop_policy
+from bot.handlers import other_handlers
+from bot.handlers import user_handlers
+from bot.utils.misc import check_eventloop_policy, check_storage_old
 
 check_eventloop_policy()
 
@@ -14,7 +15,8 @@ async def main():
     dp.include_router(user_handlers.user_router)
     dp.include_router(admin_handlers.admin_router)
     dp.include_router(misc_handlers.misc_router)
-
+    scheduler.add_job(check_storage_old, 'cron', hour=0, minute=0, second=0, args=(dp.storage,))
+    scheduler.start()
     await dp.start_polling(bot)
 
 
